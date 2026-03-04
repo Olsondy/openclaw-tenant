e218c16851d43b69a2c0360ce324a4ce072ef4c4
Olsond <olsond.dy@gmail.com>
2026-03-04 18:43:02 +0800

    feat: add root App component with login/dashboard state machine

---
+++ packages/ui/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { isLoggedIn } from "./lib/api";
+  import Login from "./lib/Login.svelte";
+  import LicenseList from "./lib/LicenseList.svelte";
+
+  let loggedIn = $state(isLoggedIn());
+</script>
+
+{#if loggedIn}
+  <LicenseList onLogout={() => (loggedIn = false)} />
+{:else}
+  <Login onLogin={() => (loggedIn = true)} />
+{/if}
